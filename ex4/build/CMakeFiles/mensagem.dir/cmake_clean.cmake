c431c454c60104a7c5aa63b21d0656f829fdfe35
LarissaMerida <larissadasilvasantos746@gmail.com>
2019-03-15 15:21:09 -0300

    adicionando arquivos

---
+++ ex4/build/CMakeFiles/mensagem.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mensagem.dir/src/Mensagem.cpp.o"
+  "CMakeFiles/mensagem.dir/src/main.cpp.o"
+  "mensagem.pdb"
+  "mensagem"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mensagem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
